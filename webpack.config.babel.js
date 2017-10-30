a03fe8df5d576052c1fc3a37bbeb39ade167a970
Braulio <braulio.diez@gmail.com>
2017-10-30 16:30:12 +0100

    updated webpack.config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,17 +1,25 @@
-var webpack = require('webpack');
-var path = require('path');
+import webpack from 'webpack';
+import path from 'path';
 
 const NODE_ENV = process.env.NODE_ENV;
 
+
 const filename = `react-promise-tracker${NODE_ENV === 'production' ? '.min' : ''}.js`;
 
-module.exports = {
+export default {
   entry: ['./src/index.js'],
   output: {
     path: path.join(__dirname, 'dist'),
     filename,
     library: 'ReactPromiseTracker',
     libraryTarget: 'umd',
+  },
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+    }
   },
   module: {
     rules: [
