f49a0d3a4cd1c6d70101ac396d4e979da622f945
unknown <alejandro.ro.pu@gmail.com>
2017-11-05 21:34:09 +0100

    Refactored tinyemmiter.js

--- src/tinyemmiter.js
+++ src/tinyemmiter.js
@@ -2,39 +2,93 @@
 // Based on:
 // https://github.com/scottcorgan/tiny-emitter
 // class based
+
+const isEventValid = (event) => {
+  return event;
+};
+
+const getOrCreateEventSubscribersFor = (allEventSubscribers, event) => 
+  getEventSubscribersFor(event) ? getEventSubscribersFor(event) : createEventSubscribersFor(event);
+
+const getEventSubscribersFor = (allEventSubscribers, event) => allEventSubscribers[event];
+
+const createEventSubscribersFor = (allEventSubscribers, event) => (allEventSubscribers[event] = []);
+
+const subscribeCallback = (eventSubscribers, callback, context, unsubscribeAfterCalled) => {
+  callback.context = context;
+  if (unsubscribeAfterCalled) {
+    callback.off_event = true;
+  }
+  eventSubscribers.push(callback);
+};
+
+const unsubscribeCallback = (allEventSubscribers, event, callbackToRemove) => {
+  if (!callbackToRemove) {
+    delete allEventSubscribers[event];
+  } else {
+    allEventSubscribers[event] = allEventSubscribers[event].filter((callback) => callback !== callbackToRemove);
+  }
+};
+
+const signalSubscribers = (eventSubscribers, ...args) => {
+  const callbacksToUnsubscribeAfterCalled = [];
+  eventSubscribers.each((callback) => {
+    callback.apply(callback.context, [...args]);
+    if (callback.off_event) {
+      callbacksToUnsubscribeAfterCalled.push(callback);
+    }
+  });
+  return callbacksToUnsubscribeAfterCalled;
+};
+
+
 export class Emitter {
+
   emit(event, ...args) {
-    if (!event) return this;
-    for (const fn of this._e(event)) {
-      fn.apply(fn.ctx, [...args]);
-      if (fn.off_event == true) this.off(event, fn);
+    const allEventSubscribers = this;
+    if (isEventValid(event)) {
+      const eventSubscribers = getEventSubscribersFor(event);
+      if (eventSubscribers) {
+        const callbacksToUnsubscribe = signalSubscribers(eventSubscribers, [...args]);
+        callbacksToUnsubscribe.each((callback) => {
+          unsubscribeCallback(allEventSubscribers, event, callback);
+        });
+      }
     }
     return this;
   }
-  on(event, fn, ctx) {
-    if (!event) return this;
-    fn.ctx = ctx;
-    this._e(event).push(fn);
-    return this;
-  }
-  once(event, fn, ctx) {
-    if (!event) return this;
-    fn.ctx = ctx;
-    fn.off_event = true;
-    return this.on(event, fn);
-  }
-  off(event, fn) {
-    if (!event) return this;
-    if (!this[event]) return this;
-    const e = this._e(event);
-    if (!fn) {
-      delete this[event];
-      return this;
+
+  on(event, callback, context) {
+    const allEventSubscribers = this;
+    if (isEventValid(event)) {
+      const eventSubscribers = getOrCreateEventSubscribersFor(allEventSubscribers, event);
+      subscribeCallback(eventSubscribers, callback, context);
     }
-    this[event] = e.filter((f) => f != fn);
     return this;
   }
+
+  once(event, callback, context) {
+    const allEventSubscribers = this;
+    const unsubscribeAfterCalled = true;
+    if (isEventValid(event)) {
+      const eventSubscribers = getOrCreateEventSubscribersFor(allEventSubscribers, event);
+      subscribeCallback(eventSubscribers, callback, context, unsubscribeAfterCalled);
+    }
+    return this;
+  }
+
+  off(event, callback) {
+    const allEventSubscribers = this;
+    if (isEventValid(event)) {
+      const eventSubscribers = getEventSubscribersFor(event);
+      if (eventSubscribers) {
+        unsubscribeCallback(allEventSubscribers, event, callback);
+      }
+    }
+    return this;
+  }
+
   _e(e) {
-    return this[e] || (this[e] = []);
+    return getOrCreateEventSubscribersFor(e);
   }
 }
