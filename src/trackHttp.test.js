33d2eb693272c716fa75451be4ef5ba5b2def965
Braulio <braulio.diez@gmail.com>
2017-11-11 22:17:36 +0100

    multiple promises case

--- src/trackHttp.test.js
+++ src/trackHttp.test.js
@@ -25,7 +25,7 @@ describe('trackPromise', () => {
     return myPromise;
   });
 
-  test('Promise tracked, we got resolve, check that going zero emitter is sent', () => {
+  test('Promise tracked, we got resolve, check that emit is called 2 times', () => {
     // Arrange
 
     emitter.emit = jest.fn((a,b) => {
@@ -45,5 +45,32 @@ describe('trackPromise', () => {
 
     return myPromise;
   });
+
+  // Pending promise failed
+
+  test('Two Promises tracked, we got resolve on both, check that emit is called 4 times', () => {
+    // Arrange
+
+    emitter.emit = jest.fn((a,b) => {
+      return;
+    });
+
+
+    const myPromiseA =  Promise.resolve();
+    const myPromiseB =  Promise.resolve();
+    const promises = [myPromiseA, myPromiseB];
+
+    // Act
+    trackPromise(myPromiseA);
+    trackPromise(myPromiseB);
+
+    // Assert
+    Promise.all(promises).then(() => {
+      expect(emitter.emit).toHaveBeenCalledTimes(4);
+    });
+
+    return Promise.all(promises);
+  });
+
 });
 
