e7e1c8d8f1d40a1b513cb93d95c341409b7ed862
Braulio <braulio.diez@gmail.com>
2017-11-11 21:05:34 +0100

    added ignore folders and starting working with mocking

--- src/trackHttp.test.js
+++ src/trackHttp.test.js
@@ -1,12 +1,22 @@
 import {trackPromise, emitter} from './trackHttp';
 import {Emitter} from './tinyemmiter';
 
-//jest.mock('emitter', jest.fn)
+jest.mock('emitter', jest.fn)
 
 describe('trackPromise', () => {
   test('Initial case, promise emitter.emit is called', () => {
     // Arrange
-    // Act 
+    emitter.mockImplementation(
+       () => ({
+        emit: (a ,b) => {}
+       })
+    );
+
+    const myPromise = new Promise((resolve, reject) => {});
+
+    // Act
+    trackPromise(promise)
+
     // Assert
     expect(emitter.emit).toHaveBeenCalled(1);
   });
