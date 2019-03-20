2b1de3df558724275be90e48c097737bbbf4f591
Braulio <braulio.diez@gmail.com>
2019-03-20 09:15:56 +0100

    refactor config and added delay pending check

--- src/trackerHook.js
+++ src/trackerHook.js
@@ -3,12 +3,25 @@ import { emitter, promiseCounterUpdateEventId } from './trackPromise';
 import { defaultArea } from './constants';
 
 export const usePromiseTracker = (config = {area: defaultArea, delay: 0}) => {
+  const [internalPromiseInProgress, setInternalPromiseInProgress] = React.useState(false)
   const [promiseInProgress, setPromiseInProgress]= React.useState(false);
+  const latestInternalPromiseInProgress = React.useRef(internalPromiseInProgress);
 
   React.useEffect(()=> {
+    latestInternalPromiseInProgress.current = internalPromiseInProgress;
     emitter.on(promiseCounterUpdateEventId, (anyPromiseInProgress, areaAffected) => {
       if(config.area === areaAffected) {
-        setPromiseInProgress(anyPromiseInProgress);
+        setInternalPromiseInProgress(anyPromiseInProgress);
+        if(!anyPromiseInProgress) {
+          setPromiseInProgress(anyPromiseInProgress);
+        } else {
+          setTimeout(() => {
+            // Check here ref to internalPromiseInProgress
+            if(latestInternalPromiseInProgress.current === true) {
+              setPromiseInProgress(anyPromiseInProgress);
+            }
+          }, config.delay);
+        }
       }
     });
 
