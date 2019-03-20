f4a8b88ba405b0efb0ac05c54c0ac4de59ebc3ff
Braulio <braulio.diez@gmail.com>
2019-03-20 21:41:10 +0100

    cleaning up code

--- src/trackerHook.js
+++ src/trackerHook.js
@@ -1,33 +1,51 @@
-import React from 'react'
-import { emitter, promiseCounterUpdateEventId } from './trackPromise';
-import { defaultArea } from './constants';
+import React from "react";
+import { emitter, promiseCounterUpdateEventId } from "./trackPromise";
+import { defaultArea } from "./constants";
 
-export const usePromiseTracker = (config = {area: defaultArea, delay: 0}) => {
-  const [internalPromiseInProgress, setInternalPromiseInProgress] = React.useState(false)
-  const [promiseInProgress, setPromiseInProgress]= React.useState(false);
-  const latestInternalPromiseInProgress = React.useRef(internalPromiseInProgress);
+export const usePromiseTracker = (config = { area: defaultArea, delay: 0 }) => {
+  const [
+    internalPromiseInProgress,
+    setInternalPromiseInProgress
+  ] = React.useState(false);
+  const [promiseInProgress, setPromiseInProgress] = React.useState(false);
+  const latestInternalPromiseInProgress = React.useRef(
+    internalPromiseInProgress
+  );
 
-  React.useEffect(()=> {
-    latestInternalPromiseInProgress.current = internalPromiseInProgress;
-    emitter.on(promiseCounterUpdateEventId, (anyPromiseInProgress, areaAffected) => {
-      if(config.area === areaAffected) {
-        setInternalPromiseInProgress(anyPromiseInProgress);
-        if(!anyPromiseInProgress) {
-          setPromiseInProgress(anyPromiseInProgress);
-        } else {
-          setTimeout(() => {
-            // Check here ref to internalPromiseInProgress
-            if(latestInternalPromiseInProgress.current === true) {
-              setPromiseInProgress(anyPromiseInProgress);
-            }
-          }, config.delay);
-        }
+  const notifiyPromiseInProgress = () => {
+    // Take into account delay parameter
+    setTimeout(() => {
+      // Check here ref to internalPromiseInProgress
+      if (latestInternalPromiseInProgress.current === true) {
+        setPromiseInProgress(true);
       }
-    });
+    }, config.delay);
+  };
 
-    return () => {emitter.off(promiseCounterUpdateEventId);
+  const updatePromiseTrackerStatus = (anyPromiseInProgress, areaAffected) => {
+    if (config.area === areaAffected) {
+      setInternalPromiseInProgress(anyPromiseInProgress);
+      if (!anyPromiseInProgress) {
+        setPromiseInProgress(false);
+      } else {
+        notifiyPromiseInProgress();
+      }
     }
-  },[]);
+  };
 
-  return {promiseInProgress}
-}
+  React.useEffect(() => {
+    latestInternalPromiseInProgress.current = internalPromiseInProgress;
+    emitter.on(
+      promiseCounterUpdateEventId,
+      (anyPromiseInProgress, areaAffected) => {
+        updatePromiseTrackerStatus(anyPromiseInProgress, areaAffected);
+      }
+    );
+
+    return () => {
+      emitter.off(promiseCounterUpdateEventId);
+    };
+  }, []);
+
+  return { promiseInProgress };
+};
