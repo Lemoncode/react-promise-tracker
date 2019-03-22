e45f6b4051b69585cfcd95da0396e251506b0d4b
Braulio <braulio.diez@gmail.com>
2019-03-22 21:33:09 +0100

    added comments

--- src/trackerHook.js
+++ src/trackerHook.js
@@ -3,11 +3,18 @@ import { emitter, promiseCounterUpdateEventId } from "./trackPromise";
 import { defaultArea } from "./constants";
 
 export const usePromiseTracker = (config = { area: defaultArea, delay: 0 }) => {
+  // Internal will hold the current value
   const [
     internalPromiseInProgress,
     setInternalPromiseInProgress
   ] = React.useState(false);
+  // Promise in progress is 'public', it can be affected by the _delay_ parameter
+  // it may not show the current state
   const [promiseInProgress, setPromiseInProgress] = React.useState(false);
+
+  // We need to hold a ref to latestInternal, to check the real value on
+  // callbacks (if not we would get always the same value)
+  // more info: https://overreacted.io/a-complete-guide-to-useeffect/
   const latestInternalPromiseInProgress = React.useRef(
     internalPromiseInProgress
   );
@@ -27,6 +34,8 @@ export const usePromiseTracker = (config = { area: defaultArea, delay: 0 }) => {
   const updatePromiseTrackerStatus = (anyPromiseInProgress, areaAffected) => {
     if (config.area === areaAffected) {
       setInternalPromiseInProgress(anyPromiseInProgress);
+      // Update the ref object as well, we will check it when we need to
+      // cover the _delay_ case (setTimeout)
       latestInternalPromiseInProgress.current = anyPromiseInProgress;
       if (!anyPromiseInProgress) {
         setPromiseInProgress(false);
