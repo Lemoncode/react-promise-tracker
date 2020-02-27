a7246e2ec81fc0e762931f85e955a3011688f319
Sara Lissette <lissette.ibnz@gmail.com>
2020-02-27 14:20:41 +0000

    manuallyIncrementPromiseCounter test added

--- src/trackPromise.test.js
+++ src/trackPromise.test.js
@@ -3,7 +3,8 @@ import {
   emitter,
   getCounter,
   manuallyResetPromiseCounter,
-  manuallyDecrementPromiseCounter
+  manuallyDecrementPromiseCounter,
+  manuallyIncrementPromiseCounter
 } from "./trackPromise";
 import { defaultArea } from "./constants";
 
@@ -1115,6 +1116,427 @@ describe("manuallyDecrementPromiseCounter", () => {
       );
       expect(getCounter("testArea1")).toBe(1);
       expect(getCounter("testArea2")).toBe(1);
+    });
+  });
+});
+
+describe("manuallyIncrementPromiseCounter", () => {
+  describe("using default area", () => {
+    beforeEach(() => {
+      manuallyResetPromiseCounter();
+    });
+
+    it("On Initial case, manuallyIncrementPromiseCounter fired, promise emitter.emit is called", () => {
+      // Arrange
+      emitter.emit = jest.fn();
+
+      // Act
+      manuallyIncrementPromiseCounter();
+
+      // Assert
+      expect(emitter.emit).toHaveBeenCalledTimes(1);
+
+      expect(emitter.emit).toHaveBeenNthCalledWith(
+        1,
+        "promise-counter-update",
+        true,
+        defaultArea
+      );
+    });
+
+    it("Promise tracked and manuallyIncrementPromiseCounter fired, we got resolve, check that emit is called 3 times", done => {
+      // Arrange
+      emitter.emit = jest.fn();
+
+      const myPromise = Promise.resolve();
+
+      // Act
+      trackPromise(myPromise);
+      manuallyIncrementPromiseCounter();
+
+      // Assert
+      myPromise.then(() => {
+        expect(emitter.emit).toHaveBeenCalledTimes(3);
+
+        expect(emitter.emit).toHaveBeenNthCalledWith(
+          1,
+          "promise-counter-update",
+          true,
+          defaultArea
+        );
+
+        expect(emitter.emit).toHaveBeenNthCalledWith(
+          2,
+          "promise-counter-update",
+          true,
+          defaultArea
+        );
+
+        expect(emitter.emit).toHaveBeenNthCalledWith(
+          3,
+          "promise-counter-update",
+          true,
+          defaultArea
+        );
+
+        expect(getCounter(defaultArea)).toBe(1);
+        done();
+      });
+    });
+
+    it("Promise tracked and manuallyIncrementPromiseCounter fired, we got fail, check that emit is called 3 times", done => {
+      // Arrange
+      emitter.emit = jest.fn();
+
+      const myPromise = Promise.reject();
+
+      // Act
+      trackPromise(myPromise);
+
+      expect(getCounter(defaultArea)).toBe(1);
+      manuallyIncrementPromiseCounter();
+      expect(getCounter(defaultArea)).toBe(2);
+
+      // Assert
+
+      myPromise.catch(() => {
+        expect(emitter.emit).toHaveBeenCalledTimes(3);
+
+        expect(emitter.emit).toHaveBeenNthCalledWith(
+          1,
+          "promise-counter-update",
+          true,
+          defaultArea
+        );
+
+        expect(emitter.emit).toHaveBeenNthCalledWith(
+          2,
+          "promise-counter-update",
+          true,
+          defaultArea
+        );
+
+        expect(emitter.emit).toHaveBeenNthCalledWith(
+          3,
+          "promise-counter-update",
+          true,
+          defaultArea
+        );
+        expect(getCounter(defaultArea)).toBe(1);
+        done();
+      });
+    });
+
+    it("Two Promises tracked and manuallyIncrementPromiseCounter fired after to start the first promise, we got resolve on both, check that emit is called 5 times", done => {
+      // Arrange
+      emitter.emit = jest.fn();
+
+      const myPromiseA = Promise.resolve();
+      const myPromiseB = Promise.resolve();
+      const promises = [myPromiseA, myPromiseB];
+
+      // Act
+      trackPromise(myPromiseA);
+      expect(getCounter(defaultArea)).toBe(1);
+
+      manuallyIncrementPromiseCounter();
+      expect(getCounter(defaultArea)).toBe(2);
+
+      trackPromise(myPromiseB);
+      expect(getCounter(defaultArea)).toBe(3);
+
+      // Assert
+      Promise.all(promises).then(() => {
+        expect(emitter.emit).toHaveBeenCalledTimes(5);
+
+        expect(emitter.emit).toHaveBeenNthCalledWith(
+          1,
+          "promise-counter-update",
+          true,
+          defaultArea
+        );
+
+        expect(emitter.emit).toHaveBeenNthCalledWith(
+          2,
+          "promise-counter-update",
+          true,
+          defaultArea
+        );
+
+        expect(emitter.emit).toHaveBeenNthCalledWith(
+          3,
+          "promise-counter-update",
+          true,
+          defaultArea
+        );
+
+        expect(emitter.emit).toHaveBeenNthCalledWith(
+          4,
+          "promise-counter-update",
+          true,
+          defaultArea
+        );
+
+        expect(emitter.emit).toHaveBeenNthCalledWith(
+          5,
+          "promise-counter-update",
+          true,
+          defaultArea
+        );
+        expect(getCounter(defaultArea)).toBe(1);
+        done();
+      });
+    });
+
+    it("Two Promises tracked and manuallyIncrementPromiseCounter fired after to start the second promise, we got resolve on both, check that emit is called 5 times", done => {
+      // Arrange
+      emitter.emit = jest.fn();
+
+      const myPromiseA = Promise.resolve();
+      const myPromiseB = Promise.resolve();
+      const promises = [myPromiseA, myPromiseB];
+
+      // Act
+      trackPromise(myPromiseA);
+      trackPromise(myPromiseB);
+      expect(getCounter(defaultArea)).toBe(2);
+
+      manuallyIncrementPromiseCounter();
+      expect(getCounter(defaultArea)).toBe(3);
+
+      // Assert
+      Promise.all(promises).then(() => {
+        expect(emitter.emit).toHaveBeenCalledTimes(5);
+
+        expect(emitter.emit).toHaveBeenNthCalledWith(
+          1,
+          "promise-counter-update",
+          true,
+          defaultArea
+        );
+
+        expect(emitter.emit).toHaveBeenNthCalledWith(
+          2,
+          "promise-counter-update",
+          true,
+          defaultArea
+        );
+
+        expect(emitter.emit).toHaveBeenNthCalledWith(
+          3,
+          "promise-counter-update",
+          true,
+          defaultArea
+        );
+
+        expect(emitter.emit).toHaveBeenNthCalledWith(
+          4,
+          "promise-counter-update",
+          true,
+          defaultArea
+        );
+
+        expect(emitter.emit).toHaveBeenNthCalledWith(
+          5,
+          "promise-counter-update",
+          true,
+          defaultArea
+        );
+        expect(getCounter(defaultArea)).toBe(1);
+        done();
+      });
+    });
+
+    it("Promise returned must handle transparently the result when resolved and manuallyIncrementPromiseCounter fired", done => {
+      // Arrange
+      const expectedPromiseResult = "promise result";
+      const promise = Promise.resolve(expectedPromiseResult);
+
+      // Act
+      const trackedPromise = trackPromise(promise);
+      manuallyIncrementPromiseCounter();
+
+      // Assert
+      trackedPromise.then(trackedPromiseResult => {
+        expect(trackedPromiseResult).toEqual(expectedPromiseResult);
+        done();
+      });
+    });
+  });
+
+  describe("using custom area", () => {
+    const testArea = "testArea";
+
+    beforeEach(() => {
+      manuallyResetPromiseCounter();
+      manuallyResetPromiseCounter(testArea);
+    });
+
+    it("should call emitter.emit two time when feeding promise and area equals undefined and manuallyIncrementPromiseCounter fired", () => {
+      // Arrange
+      emitter.emit = jest.fn();
+
+      const myPromise = Promise.resolve();
+      const area = undefined;
+
+      // Act
+      trackPromise(myPromise, area);
+      manuallyIncrementPromiseCounter(area);
+
+      // Assert
+      expect(emitter.emit).toHaveBeenCalledTimes(2);
+      expect(emitter.emit).toHaveBeenNthCalledWith(
+        1,
+        "promise-counter-update",
+        true,
+        defaultArea
+      );
+      expect(emitter.emit).toHaveBeenNthCalledWith(
+        2,
+        "promise-counter-update",
+        true,
+        defaultArea
+      );
+    });
+
+    it("should call emitter.emit two time when feeding promise and area equals null and manuallyIncrementPromiseCounter fired", () => {
+      // Arrange
+      emitter.emit = jest.fn();
+
+      const myPromise = Promise.resolve();
+      const area = null;
+
+      // Act
+      trackPromise(myPromise, area);
+      manuallyIncrementPromiseCounter(area);
+
+      // Assert
+      expect(emitter.emit).toHaveBeenCalledTimes(2);
+      expect(emitter.emit).toHaveBeenNthCalledWith(
+        1,
+        "promise-counter-update",
+        true,
+        defaultArea
+      );
+      expect(emitter.emit).toHaveBeenNthCalledWith(
+        2,
+        "promise-counter-update",
+        true,
+        defaultArea
+      );
+    });
+
+    it("should call emitter.emit two time when feeding promise and area equals testArea and manuallyIncrementPromiseCounter fired", () => {
+      // Arrange
+      emitter.emit = jest.fn();
+
+      const myPromise = Promise.resolve();
+
+      // Act
+      trackPromise(myPromise, testArea);
+      manuallyIncrementPromiseCounter(testArea);
+
+      // Assert
+      expect(emitter.emit).toHaveBeenCalledTimes(2);
+      expect(emitter.emit).toHaveBeenNthCalledWith(
+        1,
+        "promise-counter-update",
+        true,
+        "testArea"
+      );
+      expect(emitter.emit).toHaveBeenNthCalledWith(
+        2,
+        "promise-counter-update",
+        true,
+        "testArea"
+      );
+    });
+
+    it("should call emitter.emit three times when feeding two promises in same area and manuallyIncrementPromiseCounter fired", () => {
+      // Arrange
+      emitter.emit = jest.fn();
+
+      const myPromise1 = Promise.resolve();
+      const myPromise2 = Promise.resolve();
+
+      // Act
+      trackPromise(myPromise1, testArea);
+      trackPromise(myPromise2, testArea);
+      manuallyIncrementPromiseCounter(testArea);
+
+      // Assert
+      expect(emitter.emit).toHaveBeenCalledTimes(3);
+      expect(emitter.emit).toHaveBeenNthCalledWith(
+        1,
+        "promise-counter-update",
+        true,
+        "testArea"
+      );
+      expect(emitter.emit).toHaveBeenNthCalledWith(
+        2,
+        "promise-counter-update",
+        true,
+        "testArea"
+      );
+      expect(emitter.emit).toHaveBeenNthCalledWith(
+        3,
+        "promise-counter-update",
+        true,
+        "testArea"
+      );
+    });
+
+    it("should call emitter.emit three times when feeding three promises in different areas and manuallyIncrementPromiseCounter fired", () => {
+      // Arrange
+      emitter.emit = jest.fn();
+
+      const myPromise1 = Promise.resolve();
+      const myPromise2 = Promise.resolve();
+      const myPromise3 = Promise.resolve();
+
+      const area1 = "testArea1";
+      const area2 = "testArea2";
+
+      // Act
+      trackPromise(myPromise1, area1);
+      trackPromise(myPromise2, area2);
+      trackPromise(myPromise3, area2);
+      expect(getCounter(area1)).toBe(1);
+      expect(getCounter(area2)).toBe(2);
+      manuallyIncrementPromiseCounter(area2);
+
+      // Assert
+      expect(emitter.emit).toHaveBeenCalledTimes(4);
+      expect(emitter.emit).toHaveBeenNthCalledWith(
+        1,
+        "promise-counter-update",
+        true,
+        "testArea1"
+      );
+      expect(emitter.emit).toHaveBeenNthCalledWith(
+        2,
+        "promise-counter-update",
+        true,
+        "testArea2"
+      );
+      expect(emitter.emit).toHaveBeenNthCalledWith(
+        3,
+        "promise-counter-update",
+        true,
+        "testArea2"
+      );
+      expect(emitter.emit).toHaveBeenNthCalledWith(
+        4,
+        "promise-counter-update",
+        true,
+        "testArea2"
+      );
+      expect(getCounter(area1)).toBe(1);
+      expect(getCounter(area2)).toBe(3);
+
+      // Reset
+      manuallyResetPromiseCounter(area1);
+      manuallyResetPromiseCounter(area2);
     });
   });
 });
