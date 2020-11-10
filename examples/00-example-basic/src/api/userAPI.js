194f69d190e93c76cb12b464dadb4f3d15d05734
Kiran Kumar <kiran22tulashaku7@gmail.com>
2020-11-10 18:35:25 +0530

    Async functions
    change from then/catch to async/await and try/catch (modern promise methods)

--- examples/00-example-basic/src/api/userAPI.js
+++ examples/00-example-basic/src/api/userAPI.js
@@ -1,7 +1,14 @@
 import { fetchWithDelay } from './fetch';
 const url = 'https://jsonplaceholder.typicode.com/users';
 
-const fetchUsers = () => fetchWithDelay(url);
+const fetchUsers = async () =>{ 
+try {
+  const users =await fetchWithDelay(url);
+  return users;
+} catch(error){
+  throw new Error(error);
+}
+}
 
 export const userAPI = {
   fetchUsers,
