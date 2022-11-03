6ddd0ecb34549818c7a842736823e321921c0282
daniel.sanchez <daniel.sanchez@lemoncode.net>
2022-11-03 15:10:02 +0100

    update webpack

--- config/webpack/webpack.prod.js
+++ config/webpack/webpack.prod.js
@@ -1,11 +1,10 @@
-const merge = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = (env, argv) => merge(commonConfig(env, argv), {
   mode: 'production',
-  devtool: 'none',
   plugins: [
     new BundleAnalyzerPlugin({
       analyzerMode: "static",
