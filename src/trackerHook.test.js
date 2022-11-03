1400b98ecd639758c39d5a4fe9bddf95ee471912
daniel.sanchez <daniel.sanchez@lemoncode.net>
2022-11-03 14:29:14 +0100

    Fix trackerHook specs

--- src/trackerHook.test.js
+++ src/trackerHook.test.js
@@ -1,159 +1,105 @@
-import React from "react";
-import { usePromiseTracker } from "./trackerHook";
-import * as trackPromiseAPI from "./trackPromise";
-import { act } from "react-dom/test-utils"; // ES6
+import React from 'react';
+import { renderHook, render, screen, act } from '@testing-library/react';
+import * as trackPromiseAPI from './trackPromise';
+import { usePromiseTracker } from './trackerHook';
 
-describe("trackerHook", () => {
-  describe("Initial Status", () => {
-    it("renders without crashing", () => {
+describe('trackerHook', () => {
+  describe('Initial Status', () => {
+    it('should return promiseInProgress equals false on calls it', () => {
       // Arrange
-      const TestSpinnerComponent = props => {
-        const { promiseInProgress } = usePromiseTracker();
-
-        return <span>test</span>;
-      };
 
       // Act
-      const component = mount(<TestSpinnerComponent />);
+      const { result } = renderHook(() => usePromiseTracker());
 
       // Assert
-      expect(component).toMatchSnapshot();
+      expect(result.current).toEqual({ promiseInProgress: false });
     });
 
-    it("should render component with trackedPromiseInProgress equals false when counter is 0", () => {
+    it('should return promiseInProgress equals false when counter is 0', () => {
       // Arrange
-      const TestSpinnerComponent = props => {
-        const { promiseInProgress } = usePromiseTracker();
-
-        return <span>test</span>;
-      };
-
-      trackPromiseAPI.getCounter = jest.fn().mockImplementation(() => 0);
+      const getCounterStub = jest
+        .spyOn(trackPromiseAPI, 'getCounter')
+        .mockImplementation(() => 0);
 
       // Act
-      const component = mount(<TestSpinnerComponent />);
+      const { result } = renderHook(() => usePromiseTracker());
 
       // Assert
-      expect(trackPromiseAPI.getCounter).toHaveBeenCalled();
-      expect(component).toMatchSnapshot();
+      expect(getCounterStub).toHaveBeenCalled();
+      expect(result.current).toEqual({ promiseInProgress: false });
     });
 
-    it("should render component with trackedPromiseInProgress equals false when counter is 0 and emit event with progress equals false to different area", () => {
+    it('should return promiseInProgress equals false when counter is 0 and emit event with progress equals false to different area', () => {
       // Arrange
-      const TestSpinnerComponent = props => {
-        const { promiseInProgress } = usePromiseTracker();
-
-        return <span>test</span>;
-      };
-
-      trackPromiseAPI.getCounter = jest.fn().mockImplementation(() => 0);
+      const getCounterStub = jest
+        .spyOn(trackPromiseAPI, 'getCounter')
+        .mockImplementation(() => 0);
 
       const progress = false;
-      const area = "otherArea";
+      const area = 'otherArea';
       const emitterStub = jest
-        .spyOn(trackPromiseAPI.emitter, "on")
+        .spyOn(trackPromiseAPI.emitter, 'on')
         .mockImplementation((id, callback) => callback(progress, area));
 
       // Act
-      const component = mount(<TestSpinnerComponent />);
+      const { result } = renderHook(() => usePromiseTracker());
 
       // Assert
-      expect(trackPromiseAPI.getCounter).toHaveBeenCalled();
-      expect(component).toMatchSnapshot();
+      expect(getCounterStub).toHaveBeenCalled();
+      expect(result.current).toEqual({ promiseInProgress: false });
     });
 
-    it("should render component with trackedPromiseInProgress equals false when counter is 0 and emit event with progress equals true to different area", () => {
+    it('should return promiseInProgress equals false when counter is 0 and emit event with progress equals true to different area', () => {
       // Arrange
-      const TestSpinnerComponent = props => {
-        const { promiseInProgress } = usePromiseTracker();
-
-        return <span>test</span>;
-      };
-
-      trackPromiseAPI.getCounter = jest.fn().mockImplementation(() => 0);
+      const getCounterStub = jest
+        .spyOn(trackPromiseAPI, 'getCounter')
+        .mockImplementation(() => 0);
 
       const progress = true;
-      const area = "otherArea";
+      const area = 'otherArea';
       const emitterStub = jest
-        .spyOn(trackPromiseAPI.emitter, "on")
+        .spyOn(trackPromiseAPI.emitter, 'on')
         .mockImplementation((id, callback) => callback(progress, area));
 
       // Act
-      const component = mount(<TestSpinnerComponent />);
+      const { result } = renderHook(() => usePromiseTracker());
 
       // Assert
-
-      expect(trackPromiseAPI.getCounter).toHaveBeenCalled();
-      expect(component).toMatchSnapshot();
+      expect(getCounterStub).toHaveBeenCalled();
+      expect(result.current).toEqual({ promiseInProgress: false });
     });
 
-    it("should render <h2>No Spinner</h2> when counter is 0", () => {
+    it('should return promiseInProgress equals true when counter is 1', () => {
       // Arrange
-      const TestSpinnerComponent = props => {
-        const { promiseInProgress } = usePromiseTracker();
-
-        return (
-          <div>
-            {promiseInProgress ? <h1>SPINNER</h1> : <h2>NO SPINNER</h2>}
-          </div>
-        );
-      };
-
-      trackPromiseAPI.getCounter = jest.fn().mockImplementation(() => 0);
+      const getCounterStub = jest
+        .spyOn(trackPromiseAPI, 'getCounter')
+        .mockImplementation(() => 1);
 
       // Act
-      const component = mount(<TestSpinnerComponent />);
+      const { result } = renderHook(() => usePromiseTracker());
 
       // Assert
-
-      expect(component.find("h2")).toHaveLength(1);
-      expect(trackPromiseAPI.getCounter).toHaveBeenCalled();
+      expect(getCounterStub).toHaveBeenCalled();
+      expect(result.current).toEqual({ promiseInProgress: true });
     });
 
-    it("should render <h1>Spinner</h1> when counter is 1", () => {
+    it('should return promiseInProgress equals false and area equals "testArea" when feeding area equals "testArea" and delay equals 300', () => {
       // Arrange
-      const TestSpinnerComponent = props => {
-        const { promiseInProgress } = usePromiseTracker();
-
-        return (
-          <div>
-            {promiseInProgress ? <h1>SPINNER</h1> : <h2>NO SPINNER</h2>}
-          </div>
-        );
-      };
-
-      trackPromiseAPI.getCounter = jest.fn().mockImplementation(() => 1);
 
       // Act
-      const component = mount(<TestSpinnerComponent />);
+      const { result } = renderHook(() =>
+        usePromiseTracker({
+          area: 'testArea',
+          delay: 300,
+        })
+      );
 
       // Assert
-
-      expect(component.find("h1")).toHaveLength(1);
-      expect(trackPromiseAPI.getCounter).toHaveBeenCalled();
-    });
-
-    it('should render component with trackedPromiseInProgress equals false and area equals "testArea" when feeding area equals "testArea" and delay equals 300', () => {
-      // Arrange
-      const TestSpinnerComponent = props => {
-        const { promiseInProgress } = usePromiseTracker({
-          area: "testArea",
-          delay: 300
-        });
-
-        return <span>test</span>;
-      };
-
-      // Act
-      const component = mount(<TestSpinnerComponent />);
-
-      // Assert
-
-      expect(component).toMatchSnapshot();
+      expect(result.current).toEqual({ promiseInProgress: false });
     });
   });
 
-  describe("Handling delay timeouts", () => {
+  describe('Handling delay timeouts', () => {
     beforeEach(() => {
       jest.useFakeTimers();
     });
@@ -162,11 +108,11 @@ describe("trackerHook", () => {
       jest.useRealTimers();
     });
 
-    it("should render <h1>NO SPINNER</h2> when counter is 1 but delay is set to 200 (before timing out)", done => {
+    it('should return promiseInProgress equals false when counter is 1 but delay is set to 200 (before timing out)', async () => {
       // Arrange
       let TestSpinnerComponent = null;
       act(() => {
-        TestSpinnerComponent = props => {
+        TestSpinnerComponent = (props) => {
           // Do not show spinner in the first 200 milliseconds (delay)
           const { promiseInProgress } = usePromiseTracker({ delay: 200 });
 
@@ -176,21 +122,14 @@ describe("trackerHook", () => {
             </div>
           );
         };
-
-        trackPromiseAPI.getCounter = jest.fn().mockImplementation(() => 0);
       });
 
       // Act
-      let component = null;
-
-      act(() => {
-        component = mount(<TestSpinnerComponent />);
-      });
+      render(<TestSpinnerComponent />);
 
       // Check very beginning (no promises going on) NO SPINNER is shown
       // TODO: this assert could be skipped (move to another test)
-      expect(component.text()).toEqual("NO SPINNER");
-      expect(trackPromiseAPI.getCounter).toHaveBeenCalled();
+      expect(await screen.findByText('NO SPINNER')).toBeInTheDocument();
 
       // Assert
       // This promise will resolved after 1 seconds, by doing this
@@ -198,58 +137,37 @@ describe("trackerHook", () => {
       // [0] first 200ms spinner won't be shown (text NOSPINNER)
       // [1] after 200ms spinner will be shown (text SPINNER)
       // [2] after 1000ms spinner will be hidded again (text NOSPINNER)
-      let myFakePromise = null;
-
-      act(() => {
-        myFakePromise = new Promise(resolve => {
-          setTimeout(() => {
-            resolve(true);
-          }, 1000);
-        });
+      const myFakePromise = new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(true);
+        }, 1000);
       });
 
-      act(() => {
-        trackPromiseAPI.trackPromise(myFakePromise);
+      trackPromiseAPI.trackPromise(myFakePromise);
 
-        // Runs all pending timers. whether it's a second from now or a year.
-        // https://jestjs.io/docs/en/timer-mocks.html
-        //jest.advanceTimersByTime(300);
-      });
-
-      act(() => {
-        // Runs all pending timers. whether it's a second from now or a year.
-        // https://jestjs.io/docs/en/timer-mocks.html
-        jest.advanceTimersByTime(100);
-      });
+      // Runs all pending timers. whether it's a second from now or a year.
+      // https://jestjs.io/docs/en/timer-mocks.html
+      jest.advanceTimersByTime(100);
 
       // [0] first 200ms spinner won't be shown (text NOSPINNER)
-      expect(component.text()).toEqual("NO SPINNER");
+      expect(await screen.findByText('NO SPINNER')).toBeInTheDocument();
 
-      act(() => {
-        // Runs all pending timers. whether it's a second from now or a year.
-        // https://jestjs.io/docs/en/timer-mocks.html
-        jest.advanceTimersByTime(300);
-      });
+      // Runs all pending timers. whether it's a second from now or a year.
+      // https://jestjs.io/docs/en/timer-mocks.html
+      jest.advanceTimersByTime(300);
 
       // Before the promise get's resolved
       // [1] after 200ms spinner will be shown (text SPINNER)
-      expect(component.text()).toEqual("SPINNER");
+      expect(await screen.findByText('SPINNER')).toBeInTheDocument();
 
       // After the promise get's resolved
-
-      act(() => {
-        jest.runAllTimers();
-      });
+      jest.runAllTimers();
 
       // [2] after 1000ms spinner will be hidded again (text NOSPINNER)
       // Wait for fakePromise (simulated ajax call) to be completed
       // no spinner should be shown
-      act(() => {
-        myFakePromise.then(() => {
-          expect(component.text()).toEqual("NO SPINNER");
-          done();
-        });
-      });
+      await myFakePromise;
+      expect(await screen.findByText('NO SPINNER')).toBeInTheDocument();
     });
   });
 });
